f8c15349b5577954fafa5a590909bce4ba242c80
manojeluri <manojeluri21@gmail.com>
2021-11-23 09:56:37 -0600

    prototype

--- Object Oriented JavaScript/objects.js
+++ Object Oriented JavaScript/objects.js
@@ -1,43 +1,20 @@
-class User{
-    constructor(email, name){
-        this.email = email;
-        this.name = name;
-        this.score = 0;
-    }
-    login(){
-        console.log(this.email, ' has just logged in');
-        return this;
-    }
-    logout(){
-        console.log(this.email, ' has just logged out');
-        return this;
-    }
-    updateScore(){
-        this.score++;
-        console.log(this.email, " score is ", this.score);
-        return this;
-    }
+function User(email,name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
 }
-class Admin extends User{
-    deleteUser(user){
-        users = users.filter(u =>{
-            return u.email != user.email;
-        })
-    }
+
+User.prototype.login = function(){
+    this.online = true;
+    console.log(this.email,"has logged in");
+}
+User.prototype.logout = function(){
+    this.online = false;
+    console.log(this.email,"has logged out");
 }
 
 var userOne = new User('ryu@ninjas.com', "Ryu");
 var userTwo = new User('yoshi@gmail.com', 'Yoshi');
-var admin = new Admin('shaun@ninjas.com','Shaun');
 
-userOne.login().updateScore().updateScore().logout();
-users = [userOne, userTwo, admin];
-
-admin.deleteUser(userTwo);
-console.log(users)
-
-//the new keyword
-//- creates a new empty object{}
-// sets the value of this to be the new empty object
-// calls the constructor method
-
+console.log(userOne);
+userTwo.login();
